1762b63728106cab6ed8b605309c3ec87b7e4eba
lynfee <lkh3658418@qq.com>
2015-08-22 10:32:59 +0800

    sdfsdf

---
+++ src/components/OtherUserPhoneList.jsx
@@ -0,0 +1,35 @@
+define(function(require, exports, module){
+
+  var {React} = require('module!../../../libReact/src/main'),
+      {ListGroup, ListGroupItem, Alert} = require('module!../../../libReactBootstrap/src/main');
+
+  module.exports = React.createClass({
+
+
+    propTypes: {
+      otherUserPhones: React.PropTypes.object
+    },
+
+    render(){
+      var otherUserPhones = this.props.otherUserPhones || [];
+      var hasOtherUserPhones = otherUserPhones.length > 0;
+
+      if(hasOtherUserPhones) {
+        return <div>
+          <Alert bsStyle='warning'>
+            <strong>Other User's Phones!</strong>
+          </Alert>
+          <ListGroup>
+          {otherUserPhones.map((otherUserPhone)=> {
+            return (<ListGroupItem href='#link1'>{otherUserPhone.phoneNumberInfo.formattedNumber}</ListGroupItem>);
+          })}
+        </ListGroup></div>;
+      } else {
+        return null
+      }
+
+    }
+
+  });
+
+});
